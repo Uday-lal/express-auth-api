cbf60db1210fad2e6c37b2c51328741acd645ccd
Uday-lal <uday014@gmail.com>
2021-08-31 01:04:45 +0530

    Add session to the api

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
 const app = express();
 let Users = require("./model");
 
 Users = new Users();
+const corsOptions = {
+   origin:'*', 
+   credentials:true,
+   optionSuccessStatus:200,
+}
 
+app.use(cors(corsOptions))
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(session({
+	secret: "secret_key",
+	resave: false,
+	saveUnintialized: false
+}));
+
 
 
 app.post("/register", (req, res) => {
@@ -14,7 +28,7 @@ app.post("/register", (req, res) => {
 	const password = req.body.password;
 	if (username && email && password) {
 		Users.create({username: username, email: email, password: password});
-		res.send({message: "Users is creates"});
+		res.send({message: "Users is created"});
 	} else {
 		res.send({message: "We don't have enough information to create a user"});
 	}
@@ -31,12 +45,16 @@ app.post("/login", (req, res) => {
   			let user_data = snapshot.docs[0].data();
   			if (user_data.password == password) {
   				user_data.id = snapshot.docs[0].id;
-  				res.send(
+  				res.status(200).send(
   					{
-  						message: "You'll able to login",
+  						message: "You'll are login",
   						user_data: user_data
   					}
   				);
+  				req.session.username = user_data.username;
+  				req.session.email = user_data.email;
+  				req.session.user_id = user_data.id;
+  				req.session.save();
   			} else {
   				res.send({message: "Password is incorrect"});
   			}
@@ -47,10 +65,13 @@ app.post("/login", (req, res) => {
 });
 
 app.put("/change_password", (req, res) => {
-  let user_id = req.body.user_id;
-  let updated_password = req.body.updated_password;
-  Users.update(user_id, {password: updated_password});
-  res.send({message: "Update successfull"})
+	let user_id = req.session.user_id;
+	if (user_id) {
+	  let updated_password = req.body.updated_password;
+	  Users.update(user_id, {password: updated_password});
+	} else {
+		res.send({message: "You're not loged in"});
+	}
 });
 
 app.listen(8080, () => console.log("Server listening on port 8080..."));
