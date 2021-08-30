9bb8263b1afc09b486e327e494eaff1188dd964e
Uday-lal <uday014@gmail.com>
2021-08-30 15:44:13 +0530

    Add firestore to the api and connect it with firestore

--- model.js
+++ model.js
@@ -1,7 +1,58 @@
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 
-var serviceAccount = require("FirebaseServiceAccountCred.json");
+const serviceAccount = require("./FirebaseServiceAccountCred.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+const db = admin.firestore();
+
+const table = db.collection("users");
+
+
+class Users {
+  constructor() {
+    this.users = db.collection("users");
+  }
+
+  create(data) {
+    this.users.add(data);
+  }
+
+  read(identifier) {
+    let data;
+    if (identifier) {
+      this.users
+        .where(identifier.key, "==", identifier.value)
+        .get()
+        .then((snapshot) => {
+          data = snapshot.docs[0].data();
+          data.id = snapshot.docs[0].id;
+        });
+    } else {
+      data = [];
+      this.users.get().then((snapshot) => {
+        snapshot.docs.forEach((users) => {
+          data.push(users.data());
+        });
+      });
+    }
+    return data;
+  }
+
+  update(user_id, updating_data) {
+    this.users.doc(user_id).get().then((docs) => {
+      docs.update(updating_data);
+    });
+  }
+
+  delete(user_id) {
+    this.users.doc(user_id).get().then((docs) => {
+      docs.delete();
+    });
+  }
+}
+
+
+export default Users;
